61e41e5b330c000bf0d4258cd9696444f3916eeb
The-Nikhil <nikgarhwa7890@gmail.com>
2024-03-07 18:22:38 +0530

    Home UI

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateMeeting from "seek-solution/component/CreateMeeting";
+
+function page() {
+  return (
+    <div>
+      <CreateMeeting />
+    </div>
+  );
+}
+
+export default page;
